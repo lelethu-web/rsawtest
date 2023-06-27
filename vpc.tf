2752e29bac99c00825cccafcd72422f67af00f57
lelethu <lelethukosana@aol.com>
2023-06-27 11:18:10 +0200

    test

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,17 @@
-resource "aws_vpc" "our_own_vpc" {
- cidr_block = var.vpc_cidr_block
- tags = {
-   Name = "Own VPC"
- }
-}
+#resource "aws_vpc" "our_own_vpc" {
+ #cidr_block = var.vpc_cidr_block
+ #tags = {
+ #  Name = "Own VPC"
+ #}
+#}
 
-resource "aws_subnet" "public_subnets" {
-vpc_id     = aws_vpc.our_own_vpc.id
-cidr_block = var.public_subnet_ips
-tags = {
- Name = "Public Subnet"
-}
-}
+#resource "aws_subnet" "public_subnets" {
+#vpc_id     = aws_vpc.our_own_vpc.id
+#cidr_block = var.public_subnet_ips
+#tags = {
+# Name = "Public Subnet"
+#}
+#}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -33,8 +33,15 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+ # subnet_id  =  aws_subnet.public_subnets.id
 
   tags = {
     Name = "rsawebvm"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install -y epel-release",
+      "sudo yum install -y ansible",
+    ]
   }
 }
